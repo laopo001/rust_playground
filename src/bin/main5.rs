120e66e9a31d5769b228e8a0d946a162ffe8a7b3
laopo001 <353272497@qq.com>
2020-05-13 19:00:03 +0800

    fix shaderc.rs

--- src/bin/main5.rs
+++ src/bin/main5.rs
@@ -1,35 +1,32 @@
 #[derive(Debug)]
 struct A {
-  name:i32
+    name: i32,
 }
 #[derive(Debug)]
 struct B {
-  a:A
+    a: A,
 }
 impl B {
-  fn new(mut a:A) -> B{
-    a.name =123;
-    return B{
-      a
-    };
-  }
+    fn new(mut a: A) -> B {
+        a.name = 123;
+        return B { a };
+    }
 }
-fn run( a:Box<A>){
-   dbg!(&a as *const A);
-  a.name =123;
-  dbg!(a.name);
+fn run(mut a: Box<A>) {
+    dbg!(&a as *const Box<A>);
+    a.name = 123;
+    dbg!(a.name);
 }
 
 fn main() {
-  let  a = Box::new(A{name:0});
-   dbg!(&a as *const A);
-  run(a);
- 
-  // let a_p =&a as *const A;
-  // let b = B::new(a);
-  // unsafe {
-  //  dbg!((*a_p).name);
-  // }
-  // dbg!(b);
-}
+    let a = Box::new(A { name: 0 });
+    dbg!(&a as *const Box<A>);
+    run(a);
 
+    // let a_p =&a as *const A;
+    // let b = B::new(a);
+    // unsafe {
+    //  dbg!((*a_p).name);
+    // }
+    // dbg!(b);
+}
--- src/bin/shaderc.rs
+++ src/bin/shaderc.rs
@@ -1,18 +1,32 @@
 use shaderc;
+fn main() {
+    let source = "#version 310 es\n void EP() {}";
 
-let source = "#version 310 es\n void EP() {}";
+    let mut compiler = shaderc::Compiler::new().unwrap();
+    let mut options = shaderc::CompileOptions::new().unwrap();
+    options.add_macro_definition("EP", Some("main"));
+    let binary_result = compiler
+        .compile_into_spirv(
+            source,
+            shaderc::ShaderKind::Vertex,
+            "shader.glsl",
+            "main",
+            Some(&options),
+        )
+        .unwrap();
 
-let mut compiler = shaderc::Compiler::new().unwrap();
-let mut options = shaderc::CompileOptions::new().unwrap();
-options.add_macro_definition("EP", Some("main"));
-let binary_result = compiler.compile_into_spirv(
-    source, shaderc::ShaderKind::Vertex,
-    "shader.glsl", "main", Some(&options)).unwrap();
+    assert_eq!(Some(&0x07230203), binary_result.as_binary().first());
 
-assert_eq!(Some(&0x07230203), binary_result.as_binary().first());
+    let text_result = compiler
+        .compile_into_spirv_assembly(
+            source,
+            shaderc::ShaderKind::Vertex,
+            "shader.glsl",
+            "main",
+            Some(&options),
+        )
+        .unwrap();
 
-let text_result = compiler.compile_into_spirv_assembly(
-    source, shaderc::ShaderKind::Vertex,
-    "shader.glsl", "main", Some(&options)).unwrap();
-
-assert!(text_result.as_text().starts_with("; SPIR-V\n"));
+    assert!(text_result.as_text().starts_with("; SPIR-V\n"));
+    dbg!(&text_result.as_text());
+}
